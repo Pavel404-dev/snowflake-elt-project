9d6392ac5e9d0e05e0e29d4bb14f2fe8bc5b7772
KoValsKeY <vladyslav.kovalov@student.ukf.sk>
2026-01-06 18:25:27 +0100

    dodane vsetko ine

--- sql/dotazy.sql
+++ sql/dotazy.sql
@@ -6,10 +6,17 @@ CREATE TABLE FACT_ESTATE_METRICS (
   date_key int,
   seller_key int,
   estimate_price float,
-  annual_tax_amount float ,
-  page_view_count float ,
-  rent_estimate float ,
-  included_furniture bool
+  annual_tax_amount float,
+  page_view_count int,
+  rent_estimate float,
+  included_furniture boolean,
+  monthly_hoa_fee float,
+  tax_assessed_value float,
+  price_per_square_foot float,
+  deposit_fee_min float,
+  climate_flood_risk_value float,
+  climate_fire_risk_value float,
+  favorite_count int
 );
 
 CREATE TABLE DIM_PROPERTY_DETAILS (
@@ -23,9 +30,16 @@ CREATE TABLE DIM_PROPERTY_DETAILS (
   structure_type varchar(255),
   roof_type varchar(255),
   heating_source varchar(255),
-  is_new bool,
-  description varchar(255),
-  area float
+  is_new boolean,
+  description text,
+  area float,
+  bedrooms float,
+  bathrooms float,
+  mls_id varchar(255),
+  parcel_id varchar(255),
+  lot_size_dimensions varchar(255),
+  architectural_style varchar(255),
+  has_attached_garage boolean
 );
 
 CREATE TABLE DIM_BUILDING_INFO (
@@ -38,7 +52,14 @@ CREATE TABLE DIM_BUILDING_INFO (
   has_swimming_pool boolean,
   has_elevator boolean,
   is_smoke_free boolean,
-  transit_score int
+  transit_score int,
+  has_gym boolean,
+  has_pet_park boolean,
+  has_24hr_maintenance boolean,
+  is_student_housing boolean,
+  is_senior_housing boolean,
+  pet_policy_description text,
+  security_features varchar(255)
 );
 
 CREATE TABLE DIM_LOCATION (
@@ -51,7 +72,14 @@ CREATE TABLE DIM_LOCATION (
   home_number varchar(255),
   street_name varchar(255),
   latitude float,
-  longitude float
+  longitude float,
+  elementary_school varchar(255),
+  middle_school varchar(255),
+  high_school varchar(255),
+  walk_score int,
+  walk_score_description varchar(255),
+  climate_fema_zone varchar(255),
+  climate_risk_label varchar(255)
 );
 
 CREATE TABLE DIM_DATE (
@@ -60,13 +88,13 @@ CREATE TABLE DIM_DATE (
   year int,
   quarter int,
   month int,
-  month_name varchar(255), 
-  day_of_week int ,
-  day_name varchar(255) ,
-  week_of_year int ,
-  is_weekend boolean ,
-  is_holiday boolean ,
-  fiscal_quarter int 
+  month_name varchar(255),
+  day_of_week int,
+  day_name varchar(255),
+  week_of_year int,
+  is_weekend boolean,
+  is_holiday boolean,
+  fiscal_quarter int
 );
 
 CREATE TABLE DIM_SELLER (
@@ -74,11 +102,11 @@ CREATE TABLE DIM_SELLER (
   name varchar(255),
   surname varchar(255),
   phone_number varchar(255),
-  agency_name varchar(255), 
+  agency_name varchar(255),
   agent_license_number varchar(255),
-  agent_email varchar(255) ,
-  brokerage_name varchar(255) ,
-  broker_phone_number varchar(255) ,
+  agent_email varchar(255),
+  brokerage_name varchar(255),
+  broker_phone_number varchar(255),
   is_premier_agent boolean
 );
 
