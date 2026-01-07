d38160b0c5838042c1af220e93bb5003bf162974
KoValsKeY <vladyslav.kovalov@student.ukf.sk>
2026-01-07 17:56:42 +0100

    redacted final version of stage

--- sql/dotazy.sql
+++ sql/dotazy.sql
@@ -133,34 +133,40 @@ ALTER TABLE FACT_ESTATE_METRICS ADD FOREIGN KEY (seller_key) REFERENCES DIM_SELL
 
 
 
-CREATE OR REPLACE TABLE STAGING_REAL_ESTATE_FULL AS
+-- Вказуємо створювати таблицю у ВАШІЙ базі даних
+CREATE DATABASE IF NOT EXISTS LION_MY_LAST_PROJECT;
+
+CREATE SCHEMA IF NOT EXISTS LION_MY_LAST_PROJECT.STAGING;
+
+
+CREATE OR REPLACE TABLE LION_MY_LAST_PROJECT.STAGING.STAGING_REAL_ESTATE_FULL AS
 SELECT DISTINCT
-    -- === ІДЕНТИФІКАТОРИ ТА ЗВ'ЯЗКИ ===
+    -- ІДЕНТИФІКАТОРИ
     p.ZILLOW_ZPID AS property_zpid,
     p.ID AS property_internal_id,
     p.BUILDING_KEY,
     b.BUILDING_ZPID AS building_zpid,
     
-    -- === ДЛЯ ТАБЛИЦІ ФАКТІВ (FACT_ESTATE_METRICS) ===
+    -- МЕТРИКИ
     p.PRICE AS estimate_price,
     p.RESO_FACTS_TAX_ANNUAL_AMOUNT AS annual_tax_amount,
     p.ZILLOW_PAGE_VIEW_COUNT AS page_view_count,
     p.ZILLOW_RENT_ZESTIMATE AS rent_estimate,
-    b.BUILDING_IS_FURNISHED AS included_furniture, -- Меблі частіше відносяться до опису будинку/типу оренди
+    b.BUILDING_IS_FURNISHED AS included_furniture,
     p.MONTHLY_HOA_FEE,
     p.RESO_FACTS_TAX_ASSESSED_VALUE AS tax_assessed_value,
     p.RESO_FACTS_PROPERTY_PRICE_PER_SQUARE_FOOT AS price_per_square_foot,
     b.BUILDING_DEPOSIT_FEE_MIN AS deposit_fee_min,
-    p.CLIMATE_FLOOD_RISK_VALUE,
-    p.CLIMATE_FIRE_RISK_VALUE,
+    p.climate_climate_flood_risk_value as climate_flood_risk_value,
+    p.climate_CLIMATE_FIRE_RISK_VALUE as climate_fire_risk_value,
     p.ZILLOW_FAVORITE_COUNT AS favorite_count,
 
-    -- === ДЛЯ DIM_PROPERTY_DETAILS (Деталі квартири) ===
+    -- ДЕТАЛІ КВАРТИРИ
     p.HOME_TYPE,
     p.HOME_STATUS,
-    p.YEAR_BUILT, -- або p.RESO_FACTS_STRUCTURE_YEAR_BUILT
+    p.YEAR_BUILT,
     p.RESO_FACTS_STRUCTURE_STORIES_TOTAL AS total_stories_property,
-    p.RESO_FACTS_AMENITY_ROOMS AS room_count_raw, -- Тут може бути JSON або Text
+    p.RESO_FACTS_AMENITY_ROOMS AS room_count_raw,
     p.RESO_FACTS_STRUCTURE_TYPE AS structure_type,
     p.RESO_FACTS_STRUCTURE_ROOF_TYPE AS roof_type,
     p.RESO_FACTS_UTILITY_HEATING AS heating_source,
@@ -175,26 +181,22 @@ SELECT DISTINCT
     p.RESO_FACTS_STRUCTURE_ARCHITECTURAL_STYLE AS architectural_style,
     p.RESO_FACTS_STRUCTURE_HAS_ATTACHED_GARAGE AS has_attached_garage,
 
-    -- === ДЛЯ DIM_BUILDING_INFO (Інфо про ЖК/Будинок) ===
+    -- БУДІВЛЯ
     b.BUILDING_NAME,
     b.BUILDING_TYPE,
     b.BUILDING_UNIT_COUNT AS unit_count,
-    b.BUILDING_WALK_SCORE AS building_walk_score_raw, -- Можливо знадобиться тут
-    -- Примітка: total_stories_in_building часто береться з RESO фактів будинку, якщо в B немає колонки
     b.BUILDING_HAS_SWIMMING_POOL AS has_swimming_pool,
     b.BUILDING_HAS_ELEVATOR AS has_elevator,
     b.BUILDING_IS_SMOKE_FREE AS is_smoke_free,
     b.BUILDING_TRANSIT_SCORE AS transit_score,
-    -- gym та pet_park часто є в amenities JSON, але якщо є окремі колонки:
     b.BUILDING_HAS_PET_PARK AS has_pet_park, 
     b.BUILDING_HAS_24HR_MAINTENANCE AS has_24hr_maintenance,
     b.BUILDING_IS_STUDENT_HOUSING AS is_student_housing,
     b.BUILDING_IS_SENIOR_HOUSING AS is_senior_housing,
     b.BUILDING_PET_POLICY_DESCRIPTION AS pet_policy_description,
     b.BUILDING_SECURITY_TYPES AS security_features,
-    -- Якщо є окрема колонка для спортзалу в B, додайте її, інакше вона в amenities
 
-    -- === ДЛЯ DIM_LOCATION (Локація) ===
+    -- ЛОКАЦІЯ
     p.PROPERTY_CITY AS city,
     p.PROPERTY_STATE AS state_name,
     p.PROPERTY_ZIPCODE AS zip_code,
@@ -209,17 +211,15 @@ SELECT DISTINCT
     p.RESO_FACTS_PROPERTY_HIGH_SCHOOL AS high_school,
     b.BUILDING_WALK_SCORE AS walk_score,
     b.BUILDING_WALK_SCORE_DESCRIPTION AS walk_score_description,
-    p.CLIMATE_FEMA_ZONE,
-    p.CLIMATE_FLOOD_RISK_LABEL AS climate_risk_label, -- Або fire_risk_label
+    p.climate_CLIMATE_FEMA_ZONE as climate_fema_zone,
+    p.climate_CLIMATE_FLOOD_RISK_LABEL AS climate_risk_label,
 
-    -- === ДЛЯ DIM_DATE (Час) ===
-    -- Беремо дату створення або публікації як основну дату події
+    -- ДАТА
     p.CREATED_TS, 
     p.DATE_POSTED_STRING,
 
-    -- === ДЛЯ DIM_SELLER (Продавець) ===
+    -- ПРОДАВЕЦЬ
     p.ATTRIBUTION_AGENT_NAME AS seller_name,
-    -- surname треба буде витягувати з name пізніше
     p.ATTRIBUTION_AGENT_PHONE_NUMBER AS seller_phone,
     p.ATTRIBUTION_BROKER_NAME AS agency_name,
     p.ATTRIBUTION_AGENT_LICENSE_NUMBER AS agent_license,
@@ -228,6 +228,7 @@ SELECT DISTINCT
     p.ATTRIBUTION_BROKER_PHONE_NUMBER AS broker_phone,
     p.ZILLOW_IS_PREMIER_BUILDER AS is_premier_agent
 
-FROM PROPERTIES p
-LEFT JOIN BUILDINGS b 
+-- Вказуємо повний шлях до джерел (Database.Schema.Table)
+FROM LION_DB_PROJECT_F.PUBLIC.PROPERTIES p
+LEFT JOIN LION_DB_PROJECT_F.PUBLIC.BUILDINGS b 
     ON p.BUILDING_KEY = b.BUILDING_KEY;
