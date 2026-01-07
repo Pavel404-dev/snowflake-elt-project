6f02da2c4d07501e66942ae84eae7509593493b0
KoValsKeY <vladyslav.kovalov@student.ukf.sk>
2026-01-07 18:27:22 +0100

    redacted readme
    redacted value types, redacted foreign keys for snowflake

--- sql/dotazy.sql
+++ sql/dotazy.sql
@@ -66,10 +66,10 @@ CREATE TABLE DIM_LOCATION (
   location_key int PRIMARY KEY AUTO_INCREMENT,
   city varchar(255),
   state_name varchar(255),
-  zip_code varchar(255),
+  zip_code varchar(50),
   county varchar(255),
   neighborhood varchar(255),
-  home_number varchar(255),
+  home_number varchar(50),
   street_name varchar(255),
   latitude float,
   longitude float,
@@ -88,9 +88,9 @@ CREATE TABLE DIM_DATE (
   year int,
   quarter int,
   month int,
-  month_name varchar(255),
+  month_name varchar(20),
   day_of_week int,
-  day_name varchar(255),
+  day_name varchar(20),
   week_of_year int,
   is_weekend boolean,
   is_holiday boolean,
@@ -101,24 +101,21 @@ CREATE TABLE DIM_SELLER (
   seller_key int PRIMARY KEY,
   name varchar(255),
   surname varchar(255),
-  phone_number varchar(255),
+  phone_number varchar(50),
   agency_name varchar(255),
-  agent_license_number varchar(255),
+  agent_license_number varchar(100),
   agent_email varchar(255),
   brokerage_name varchar(255),
-  broker_phone_number varchar(255),
+  broker_phone_number varchar(50),
   is_premier_agent boolean
 );
-
-ALTER TABLE FACT_ESTATE_METRICS ADD FOREIGN KEY (property_key) REFERENCES DIM_PROPERTY_DETAILS (property_key);
-
-ALTER TABLE FACT_ESTATE_METRICS ADD FOREIGN KEY (building_key) REFERENCES DIM_BUILDING_INFO (building_key);
-
-ALTER TABLE FACT_ESTATE_METRICS ADD FOREIGN KEY (location_key) REFERENCES DIM_LOCATION (location_key);
-
-ALTER TABLE FACT_ESTATE_METRICS ADD FOREIGN KEY (date_key) REFERENCES DIM_DATE (date_key);
-
-ALTER TABLE FACT_ESTATE_METRICS ADD FOREIGN KEY (seller_key) REFERENCES DIM_SELLER (seller_key);
+  
+  -- Foreign Keys (Snowflake не блокує вставку, але це корисно для документації)
+  CONSTRAINT fk_property FOREIGN KEY (property_key) REFERENCES DIM_PROPERTY_DETAILS (property_key),
+  CONSTRAINT fk_building FOREIGN KEY (building_key) REFERENCES DIM_BUILDING_INFO (building_key),
+  CONSTRAINT fk_location FOREIGN KEY (location_key) REFERENCES DIM_LOCATION (location_key),
+  CONSTRAINT fk_date FOREIGN KEY (date_key) REFERENCES DIM_DATE (date_key),
+  CONSTRAINT fk_seller FOREIGN KEY (seller_key) REFERENCES DIM_SELLER (seller_key)
 
 
 
